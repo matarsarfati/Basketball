0fc5fe68dc23daeed4985e7a389ffb2414525cbe
Roei <roei@triggo.co>
2025-03-27 23:55:12 +0200

    fix tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss'; // import tailwindcss plugin
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // add tailwindcss plugin here
   ],
 });
